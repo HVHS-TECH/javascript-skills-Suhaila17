df98db0183d4ac6cf84ca8c858191f1d42b6b9b4
Suhaila17 <24014sa@hvhs.school.nz>
2026-04-29 00:18:28 +0000

    Apr 29, 2026, 12:18 AM

--- t02_variables.js
+++ t02_variables.js
@@ -4,10 +4,10 @@ Name of task
 
 console.log("Running t02_variables.js")
 console.log("Hello world")
-
+console.log()
 
 // Variables
-var name= Suhaila;
+var name= "Suhaila";
 var age= 15;
 var isStudent= true; 
 /****************************
