df1300ea8a8e22b624e76e37978f62783c9e4258
Suhaila17 <24014sa@hvhs.school.nz>
2026-04-29 00:07:38 +0000

    Apr 29, 2026, 12:07 AM

--- t02_variables.js
+++ t02_variables.js
@@ -4,9 +4,9 @@ Name of task
 
 console.log("Running t02_variables.js")
 console.log("Hello world")
-
+console.log(poketMoney)
 // Variables
-let pocketmoney= 3;
+let pocketMoney= 3;
 
 /****************************
 Main code
