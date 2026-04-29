0d05bd610d25829f158b9253f61e644e296ff8ce
Suhaila17 <24014sa@hvhs.school.nz>
2026-04-29 00:20:33 +0000

    Apr 29, 2026, 12:20 AM

--- t02_variables.js
+++ t02_variables.js
@@ -4,12 +4,13 @@ Name of task
 
 console.log("Running t02_variables.js")
 console.log("Hello world")
-console.log()
+console.log("you have" + pocketMoney+ "dollars")
 
 // Variables
 var name= "Suhaila";
 var age= 15;
 var isStudent= true; 
+let pocketMoney= 3;
 /****************************
 Main code
 ****************************/
