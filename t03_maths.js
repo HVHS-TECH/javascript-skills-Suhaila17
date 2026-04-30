697ce219792aa757215d8ad06324388625ca1db6
Suhaila17 <24014sa@hvhs.school.nz>
2026-04-30 01:03:23 +0000

    Apr 30, 2026, 1:03 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,31 @@
-/* Paste your code from task 2 here */
+/****************************
+Name of task
+****************************/
+
+console.log("Running t02_variables.js")
+
+// Variables
+var userName= "Suhaila";
+var userAge= 15;
+var currentYear= 2026; 
+var pocketMoney= 150;
+var num1= 10;
+var num2= 5;
+
+
+/****************************
+Main code
+****************************/
+console.log("Name:"+ userName);
+console.log("Age:"+ userAge);
+console.log("current Year:"+ currentYear);
+console.log("Poket Money:"+ pocketMoney);
+console.log("add", num1 + num2)
+console.log("subtract", num1 - num2)
+console.log("subtract", num1 * num2)
+console.log("divide", num1 / num2)
+
+
+/****************************
+Functions
+****************************/
