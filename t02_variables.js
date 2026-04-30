fbc31bed68ee92d2ec70672d81a125983acfb4ea
Suhaila17 <24014sa@hvhs.school.nz>
2026-04-30 01:07:06 +0000

    Apr 30, 2026, 1:07 AM

--- t02_variables.js
+++ t02_variables.js
@@ -9,21 +9,16 @@ var userName= "Suhaila";
 var userAge= 15;
 var currentYear= 2026; 
 var pocketMoney= 150;
-var num1= 10;
-var num2= 5;
+
 
 
 /****************************
 Main code
 ****************************/
-console.log("Name:"+ userName);
-console.log("Age:"+ userAge);
+console.log("Hi my name is"+ userName);
+console.log("As of 2026 I am"+ userAge + "years old");
 console.log("current Year:"+ currentYear);
-console.log("Poket Money:"+ pocketMoney);
-console.log("add", num1 + num2)
-console.log("subtract", num1 - num2)
-console.log("subtract", num1 * num2)
-console.log("divide", num1 / num2)
+console.log("I have"+ pocketMoney+"dollars");
 
 
 /****************************
