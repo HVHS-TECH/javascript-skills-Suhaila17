4aba87f1a67a4b820dcb2dcc075f59242dc4b369
Suhaila17 <24014sa@hvhs.school.nz>
2026-04-30 00:54:35 +0000

    Apr 30, 2026, 12:54 AM

--- t02_variables.js
+++ t02_variables.js
@@ -20,8 +20,8 @@ console.log("Name:"+ userName);
 console.log("Age:"+ userAge);
 console.log("current Year:"+ currentYear);
 console.log("Poket Money:"+ pocketMoney);
-console.log()
-
+console.log("add", num1 + num2)
+console.log("add", num1 + num2)
 
 
 /****************************
