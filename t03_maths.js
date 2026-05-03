99cf994a8733a4b75f0c9c8818e3db54c3ebc699
Suhaila17 <24014sa@hvhs.school.nz>
2026-05-03 08:40:16 +0000

    May 3, 2026, 8:40 AM

--- t03_maths.js
+++ t03_maths.js
@@ -9,23 +9,25 @@ var userName= "Suhaila";
 var userAge= 15;
 var currentYear= 2026; 
 var pocketMoney= 150;
-var num1= 10;
-var num2= 5;
 var newMoney;
+var birthYear= 2010;
+
 
 
 /****************************
 Main code
 ****************************/
 console.log("Hi my name is " + userName);
-console.log(" As of  " + currentYear+ " you are ", + userAge + " years old");
-console.log("you have ", + pocketMoney +"dollars");
-console.log("add", num1 + num2)
-console.log("subtract", num1 - num2)
-console.log("subtract", num1 * num2)
-console.log("divide", num1 / num2)
+console.log("As of  " + currentYear + " you are ", + userAge + " years old");
+console.log("you were born in " + birthYear);
+newAge= userAge+10
+console.log("In 10 years you will be " + newAge + " years old ");
+console.log("you have " + pocketMoney +" dollars");
 newMoney= pocketMoney/2
-console.log("You spend half of yor money now you have ", + newMoney)
+console.log("You spend half of yor money now you have " + newMoney + " dollars ");
+newMoney= pocketMoney/2+3
+console.log("Then you get $3, now you have " + newMoney);
+
 
 /****************************
 Functions
