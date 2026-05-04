48d98bd0595d97a54b46d290cc22f75054b1ea3e
Suhaila17 <24014sa@hvhs.school.nz>
2026-05-04 21:25:41 +0000

    May 4, 2026, 9:25 PM

--- t04_debugging.js
+++ t04_debugging.js
@@ -4,12 +4,11 @@ console.log("to4_debugging.js");
 var lenght = 8;
 var width = 10;
 var area = 80;
-var perimeter = 8 + length + 10 + width; 
-
+var perimeter = 36; 
 
 
 // Display the results
-console.log("A area of a piece of paper with a length of " + length + " and a width of " + width + " is "+ area);
+console.log("A area of a piece of paper with a length of " + lenght + " and a width of " + width + " is "+ area);
 console.log("The same piece of paper has a perimeter of "+perimeter);
 
 // Check, does this result look sensible
