71ff71a6cecaa420e553a2354e8f129b0dcf1330
Suhaila17 <24014sa@hvhs.school.nz>
2026-05-12 22:03:45 +0000

    May 12, 2026, 10:03 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -8,3 +8,44 @@ var pocketMoney= 150;
 var newMoney;
 var birthYear= 2010;
 
+
+
+/****************************
+Main code
+****************************/
+console.log("Hi my name is " + userName);
+console.log("As of  " + currentYear + " you are ", + userAge + " years old");
+console.log("you were born in " + birthYear);
+newAge= userAge+10
+console.log("In 10 years you will be " + newAge + " years old ");
+console.log("you have " + pocketMoney +" dollars");
+newMoney= pocketMoney/2
+console.log("You spend half of yor money now you have " + newMoney + " dollars ");
+newMoney= pocketMoney/2+3
+console.log("Then you get $3, now you have " + newMoney);
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+function start() {
+OUTPUT.innerHTML = "";
+OUTPUT.innerHTML +="<h2>Added by javascript</h2>";
+OUTPUT.innerHTML +="<p>Hello " + userName + "</p>";
+OUTPUT.innerHTML +="<p>Second graph line.</p>";
+OUTPUT.innerHTML +="<p>Third graph line</p>";
+welcome();
+displayProduct("chocolate", 4);
+displayProduct("Chips", 3);
+displayProduct("Drink", 2.50);
+}
+
+/****************************
+Functions
+****************************/
+function welcome(){
+    // Add a line to the html page
+    OUTPUT.innerHTML += "<p>Welcome to the shop<p>";
+}
+
+function displayProduct(_name, _price){
+    // Add a line to the html page
+    OUTPUT.innerHTML += "<p> " + _name + " costs:  $ " + _price + "<p>";
+}
