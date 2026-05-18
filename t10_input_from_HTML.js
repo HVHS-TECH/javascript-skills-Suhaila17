720d39959f4666a10a6a1bc2d7b1fe4129aa2883
Suhaila17 <24014sa@hvhs.school.nz>
2026-05-18 21:51:29 +0000

    May 18, 2026, 9:51 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -32,7 +32,7 @@ Functions
 function getFormInput(){
     const NAME_FIELD = document.getElementById("nameField");
     let userName = NAME_FIELD.value; 
-    OUTPUT.innerHTML = "<p>Your name is " + userName + "</p>";
+    inpu.innerHTML = "<p>Your name is " + userName + "</p>";
 }
 
 function getFormInput(){
