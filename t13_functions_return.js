29a6e9793b266a0b7905625143be7af1eba4e11e
Suhaila17 <24014sa@hvhs.school.nz>
2026-05-21 03:07:53 +0000

    May 21, 2026, 3:07 AM

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -65,6 +65,7 @@ function calculateChange(_userProductPrice, _userMoney){
     OUTPUT.innerHTML += "<p> Your product price is $" + userProductPrice + "</p>" ;
     let userMoney = Number(MONEY_FIELD.value);
     OUTPUT.innerHTML += "<p> You have $" + userMoney + "</p>";
+    OUTPUT.innerHTML += ""
     let change= Number(userMoney-userProductPrice);
     OUTPUT.innerHTML += "<p> Your change is $" + change + "</p>";
 if (change < 0){
