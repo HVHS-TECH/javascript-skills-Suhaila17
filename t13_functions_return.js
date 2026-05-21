f046bf6b60b78f573857fdab2a2aa7f6bcc34be3
Suhaila17 <24014sa@hvhs.school.nz>
2026-05-21 01:20:30 +0000

    May 21, 2026, 1:20 AM

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -1,1 +1,59 @@
-/* Paste your code from the last task */
+
+/****************************
+Name of task: Input from HTML
+****************************/
+console.log("Running t12_conditionals.js")
+//Variables
+var userName= "Suhaila";
+var userAge= 15;
+var currentYear= 2026; 
+var pocketMoney= 150;
+var newMoney;
+var birthYear= 2010;
+
+/****************************
+Main
+****************************/
+const NAME_FIELD = document.getElementById("nameField");
+const AGE_FIELD = document.getElementById("ageField");
+const MONEY_FIELD = document.getElementById("moneyField");
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+
+  console.log("Your name is " + userName);
+  console.log("As of  " + currentYear + " you are ", + userAge + " years old");
+ console.log("you were born in " + birthYear);
+ newAge= userAge+10
+ console.log("In 10 years you will be " + newAge + " years old ");
+ console.log("you have " + pocketMoney +" dollars");
+ newMoney= pocketMoney/2
+ console.log("You spend half of yor money now you have " + newMoney + " dollars ");
+ newMoney= pocketMoney/2+3
+ console.log("Then you get $3, now you have " + newMoney);
+/****************************
+Functions
+****************************/
+
+function getFormInput(){
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML = "<p> Your name is " + userName + "</p>";
+    let userAge = Number(AGE_FIELD.value);
+    OUTPUT.innerHTML += "<p> Your age is " + userAge + "</p>";
+    let userMoney = Number(MONEY_FIELD.value);
+    OUTPUT.innerHTML += "<p> You have $" + userMoney + "</p>";
+
+if (18 > userAge){
+    console.log("Definitely too young");
+}
+else if (18== userAge){ 
+    console.log("You became an adult");
+}
+else{
+    console.log("You are an adult.");
+}
+
+}
+
+function calculateChange(_moneyField){
+    let
+}
